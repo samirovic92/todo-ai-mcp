f8b57ffab9813efd28b75ac6f0bf18f241edaad7
selidrissi <s.elidrissi1992@gmail.com>
2025-11-16 23:32:41 +0100

    Implement mcp server to manage todo

--- todo-mcp-server/src/main/java/com/sei/mcpserver/BeanConfiguration.java
+++ todo-mcp-server/src/main/java/com/sei/mcpserver/BeanConfiguration.java
@@ -3,8 +3,10 @@ package com.sei.mcpserver;
 import com.sei.mcpserver.domain.port.api.TodoServicePort;
 import com.sei.mcpserver.domain.port.spi.TodoPersistencePort;
 import com.sei.mcpserver.domain.service.TodoServiceImpl;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanConfiguration {
@@ -12,5 +14,10 @@ public class BeanConfiguration {
     @Bean
     public TodoServicePort todoServicePort(TodoPersistencePort todoPersistencePort) {
         return new TodoServiceImpl(todoPersistencePort);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
---
+++ todo-mcp-server/src/main/java/com/sei/mcpserver/mcp/TodoMcpTools.java
@@ -0,0 +1,52 @@
+package com.sei.mcpserver.mcp;
+
+import com.sei.mcpserver.representation.request.CreateTodoRequest;
+import com.sei.mcpserver.representation.response.TodoResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+public class TodoMcpTools {
+    private final RestTemplate restTemplate;
+    private final Integer port;
+    private final String baseUrl;
+
+    public TodoMcpTools(RestTemplate restTemplate,
+                        @Value("${server.port}") Integer port) {
+        this.restTemplate = restTemplate;
+        this.port = port;
+        this.baseUrl =  String.format("http://localhost:%s", port);
+    }
+
+    @McpTool(name = "find_todo_by_id", description = "Finds a todo item by its ID.")
+    public TodoResponse findTodoById(Long id) {
+        log.info("Getting todo with id {}", id);
+        var url = String.format("%s/todos/%s", baseUrl, id);
+        return restTemplate.getForObject(url, TodoResponse.class);
+    }
+
+    @McpTool(name = "create_todo", description = "Creates a new todo item with the given title and description.")
+    public TodoResponse createTodo(CreateTodoRequest todo) {
+        log.info("Creating todo {}", todo);
+        var url = String.format("%s/todos", baseUrl);
+        return restTemplate.postForObject(url, todo, TodoResponse.class);
+    }
+
+    @McpTool(name = "get_all_todos", description = "Retrieves all todo items.")
+    public TodoResponse[] getAllTodos() {
+        log.info("Getting all todos");
+        var url = String.format("%s/todos", baseUrl);
+        return restTemplate.getForObject(url, TodoResponse[].class);
+    }
+
+    @McpTool(name = "delete_todo", description = "Deletes a todo item by its ID.")
+    public void deleteTodo(Long id) {
+        log.info("Deleting todo with id {}", id);
+        var url = String.format("%s/todos/%s", baseUrl, id);
+        restTemplate.delete(url);
+    }
+}
