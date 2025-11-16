1bd737f585f44714c12a67604a5aa3acd2f2907d
selidrissi <s.elidrissi1992@gmail.com>
2025-11-16 19:24:53 +0100

    init mcp server app

---
+++ todo-mcp-server/src/main/java/com/sei/mcpserver/TodoMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.sei.mcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoMcpServerApplication.class, args);
+    }
+
+}
---
+++ todo-mcp-server/src/test/java/com/sei/mcpserver/TodoMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sei.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
