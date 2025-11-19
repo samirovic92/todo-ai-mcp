8bae5b2488c073c4b836958dd018d5003d455c12
selidrissi <s.elidrissi1992@gmail.com>
2025-11-19 20:39:52 +0100

    create mcp client

---
+++ todo-mcp-client/src/main/java/com/sei/mcpclient/TodoMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.sei.mcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMcpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoMcpClientApplication.class, args);
+    }
+
+}
---
+++ todo-mcp-client/src/test/java/com/sei/mcpclient/TodoMcpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sei.mcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMcpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
